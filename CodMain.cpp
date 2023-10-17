c643dfaba746b0a91913d653e9535eb6b41a1323
PDMontielC <147265912+PDMontielC@users.noreply.github.com>
2023-10-16 18:47:32 -0600

    Se agrega codificacion
    Se cargan archivos .cpp en el cual se codigica el ejercicio 8

---
+++ CodMain.cpp
@@ -0,0 +1,38 @@
+// cambio para el repositorio remoto
+#include "CodCliente.cpp"
+#include <iostream>
+using namespace std;
+main(){
+	string nit,nombres,apellidos,direccion;
+	int telefono;
+	
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	cout<<"Ingresar Nombres: ";
+	cin>>nombres;
+	cout<<"Ingresar Apellidos: ";
+	cin>>apellidos;
+	cout<<"Ingresar Direccion: ";
+	cin>>direccion;
+	cout<<"Ingresar Telefono: ";
+	cin>>telefono;
+	// instancia de un objeto
+	Cliente obj = Cliente(nombres,apellidos,direccion,telefono,nit);
+//	cout<<"Datos del Cliente"<<obj.getNit()<<", "<<obj.getNombres()<<", "<<obj.getDireccion()<<", "<<obj.getTelefono()<<endl;
+
+	obj.mostrar();
+	cout<<"Ingrese Nit: ";
+	cin>>nit;
+	obj.setNit(nit);
+	obj.mostrar();
+
+	// instanciar un objeto y utilizar get and set
+/*	Cliente obj = Cliente();
+	obj.setNit(nit);
+	obj.setNombres(nombres);
+	obj.setApellidos(apellidos);
+	obj.setDireccion(direccion);
+	obj.setTelefono(telefono);
+	obj.mostrar();
+*/
+}
